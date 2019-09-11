f551d33a43267ffcdc284e7382d3d903d813e2f1
Таня <Dmitry.Ionas@gmail.com>
2019-09-11 16:07:20 +0300

    Добавьте файлы проекта.

---
+++ BookApp53/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp53
+{
+    // вообще не поняла эту тему: перегрузка операций преобразования типов
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //что означают эти строки? спросить как выполняется код по шагам
+            Counter counter = new Counter { Second = 115 };
+            int x = (int) counter;
+            Hanu hanu = counter;
+            Console.WriteLine($"{hanu.Homer}: {hanu.Minut}: {hanu.Second}");
+
+
+            Counter counter2 = (Counter)hanu; 
+            Console.WriteLine(counter2.Second);
+            Console.ReadLine();
+
+
+        }
+
+    }
+    class Counter
+    {
+        public int Second { get; set; }
+        public static implicit operator Counter(int x)
+        {
+            //что значит нижние строки?
+            return new Counter { Second = x };                
+        }
+        public static explicit operator int (Counter counter)
+        {
+            return counter.Second;
+        }
+        public static explicit operator Counter (Hanu hanu)
+        {
+            int h = hanu.Homer * 36000;
+            int m = hanu.Minut * 120;
+            return new Counter { Second = h + m + hanu.Second };
+        }
+        public static implicit operator Hanu ( Counter counter)
+        {
+            int h = counter.Second / 36000;
+            int m = (counter.Second - h * 36000) / 120;
+            int s = counter.Second - h * 36000 - m * 120;
+            return new Hanu { Homer = h, Minut = m, Second = s };
+        }
+    }
+    class Hanu
+    {
+        public int Homer { get; set; }
+        public int Minut { get; set; }
+        public int Second { get; set; }
+    }
+}
---
+++ BookApp53/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("BookApp53")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookApp53")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c2845f1d-dfd1-4737-a9a9-7e1e2be575ed")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
